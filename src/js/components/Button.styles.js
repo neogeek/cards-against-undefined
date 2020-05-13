5c9916633ff14fb2cfe76d600e5c7515ebfb0007
Scott Doxey <neogeek0101@gmail.com>
2020-05-13 19:28:51 -0400

    Updated room code and button styles.

--- src/js/components/Button.styles.js
+++ src/js/components/Button.styles.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 export const StyledButton = styled.button`
     margin: 0.25rem;
-    padding: 0.5rem 0.75rem;
+    padding: 0.5rem 0.6rem;
+    line-height: 1;
     font-family: inherit;
     font-size: inherit;
     color: #fff;
-    background-color: #222;
     border: 2px solid #fff;
     border-radius: 0.25rem;
+    background-color: #222;
     cursor: ${props => (props.disabled ? 'default' : 'pointer')};
     appearance: none;
     opacity: ${props => (props.disabled ? 0.6 : 1)};
--- src/js/components/RoomCode.styles.js
+++ src/js/components/RoomCode.styles.js
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const StyledRoomCode = styled.div`
     margin: 0.25rem;
-    padding: 0.5rem;
+    padding: 0.5rem 0.6rem;
     color: #fff;
     line-height: 1;
-    border-radius: 0.25rem;
     border: 2px solid #fff;
+    border-radius: 0.25rem;
     background-color: #000;
     user-select: all;
 `;
